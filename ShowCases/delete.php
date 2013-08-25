5c2b237aafb49889a4534c5f352ba4e4794e9512
mostafaasf <mostafaasf@hotmail.com>
2013-08-25 13:17:13 +0200

    Finished delete case

--- ShowCases/delete.php
+++ ShowCases/delete.php
@@ -2,6 +2,12 @@
 	include '../service/actions/general.php';
 	include '../service/init.php';
 	$id = $_GET["id"];
+
+	$result = mysql_query("SELECT image FROM Cases WHERE case_id = ".$id);
+	$imageArray = mysql_fetch_array($result);
+	$image = $imageArray["image"];
+	unlink("../".$image);
+	
 	mysql_query("DELETE FROM Cases WHERE case_id = ".$id);
 	echo "done";
 ?>
