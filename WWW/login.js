0d94d8f0385a5925cd11bf6456d2f8a59f623fb6
mostafaasf <mostafaasf@hotmail.com>
2013-09-15 11:16:27 +0200

    login session tzabeet

--- WWW/login.js
+++ WWW/login.js
@@ -9,13 +9,13 @@ function logIn(){
     if (e.readyState == 4 && e.status == 200) {
       var text = e.responseText;
       if (text == "found") {
-        document.getElementById("wrong").innerHTML = "yes";
+        window.location.href = "home.html";
       } else if (text == "wrong") {
         alert("Wrong username or password.");
       };
     }
   };
-  e.open("GET", "http://192.168.1.130:8888/voluntary/service/login.php?username=" + document.getElementById("username").value +
+  e.open("GET", "http://192.168.1.103:8888/voluntary/service/login.php?username=" + document.getElementById("username").value +
   						            "&password=" + document.getElementById("password").value, true);
   e.send();
 }
